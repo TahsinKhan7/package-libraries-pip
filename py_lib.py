25368e7e824760e704ef7598c3d6e9066c5f24ef
Tahsin Khan <tahsin_k@live.co.uk>
2019-04-24 11:54:54 +0100

    tested and used standard library, random and looked up documentation

---
+++ py_lib.py
@@ -0,0 +1,11 @@
+from random import * #prints both random, randint
+
+#printing built in functions/modules:
+print(type(random))
+
+print(random())
+print(randint(10,200))
+
+#print(sample((['person1', 'Ana', 'Mother Teresa'], 3)))
+
+print(choice((['person1', 'Ana', 'Mother Teresa'], 3)))
